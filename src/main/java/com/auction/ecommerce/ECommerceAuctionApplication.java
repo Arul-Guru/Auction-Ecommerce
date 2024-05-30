95be60bd37ba174e28838373e8695f21f6ef4147
Arul-Guru <arulanandha.guru@revature.com>
2024-05-30 19:56:11 +0530

    refactored

--- src/main/java/com/auction/ecommerce/ECommerceAuctionApplication.java
+++ src/main/java/com/auction/ecommerce/ECommerceAuctionApplication.java
@@ -1,13 +1,13 @@
-package com.example.auction_ecommerce;
+package com.auction.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuctionEcommerceApplication {
+public class ECommerceAuctionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuctionEcommerceApplication.class, args);
+		SpringApplication.run(ECommerceAuctionApplication.class, args);
 	}
 
 }
--- src/test/java/com/auction/ecommerce/ECommerceAuctionApplicationTests.java
+++ src/test/java/com/auction/ecommerce/ECommerceAuctionApplicationTests.java
@@ -1,4 +1,4 @@
-package com.auction.E_Commerce_Auction;
+package com.auction.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

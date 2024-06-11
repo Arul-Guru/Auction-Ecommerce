7c50cc20b1498a0ebcf64639e89d3ad946ec6dc4
soundhar <imsoundhar7@gmail.com>
2024-06-11 15:33:02 +0530

    spring security commit with some minor changes in user and roles

--- src/main/java/com/auction/ecommerce/repository/AuctionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.auction.ecommerce.repository;
-
-import com.auction.ecommerce.model.Auction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuctionRepository extends JpaRepository<Auction, Integer> {
-}
